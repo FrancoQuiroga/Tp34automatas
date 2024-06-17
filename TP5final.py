ef7d0a27667d7cf3e6cec2ccf36f49d2d2da2877
FrancoQuiroga <fe.quiroga@alumno.um.edu.ar>
2024-06-17 19:22:44 -0300

    Funcion de inputs

--- TP5final.py
+++ TP5final.py
@@ -1,2 +1,19 @@
-def inputsusuarios():
-    ...
+class InputError(Exception):
+    pass
+
+def tomaruserinput(cantidadopciones: int)-> int: 
+#función que comprueba que el usuario haya elegido la opcion correcta 
+    #Que tiene que hacer esta funcion? Tomar inputs? Procesar los inputs? Verificar que los inputs esten bie?
+    while True:
+        opcion = ''
+        try:
+            opcion = int(input(f'Ingrese una opción(1-{cantidadopciones}): '))
+            if opcion < 1 or opcion > cantidadopciones:
+                raise InputError
+            return  opcion
+        except InputError:
+            print('Ingrese una opción válida')
+        except ValueError:
+            print('La opcion tiene que ser un número')
+
+# tomaruserinput(5)
