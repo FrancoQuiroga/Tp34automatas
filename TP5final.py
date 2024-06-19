7920e0c876e455dca360e5893c8ee067f4118e78
diego <diego@diegodeMacBook-Air.local>
2024-06-19 11:20:55 -0300

    csv, input

--- TP5final.py
+++ TP5final.py
@@ -1,5 +1,32 @@
+import pandas as pd
+
+
+# cargar datos del csv
+# el archivo no lo subi en el github pq es muy grande, y no me deja
+# hay que crear una carpeta que se llama csv , y adentro ponerl el archivo 
+def cargar_datos(archivo):
+    return pd.read_csv(archivo, low_memory=False)
+
+
+def exportar_a_exel():
+    pass
+
+
+
+archivo_csv = 'csv/export-2019-to-now-v4.csv'
+data = cargar_datos(archivo_csv)
+
+
+
+# input
+fecha_inicio = input("Ingrese la fecha de inicio (AAAA-MM-DD): ")
+fecha_fin = input("Ingrese la fecha de fin (AAAA-MM-DD): ")
+
+
+
 class InputError(Exception):
     pass
+
 
 def tomaruserinput(cantidadopciones: int)-> int: 
 #función que comprueba que el usuario haya elegido la opcion correcta 
